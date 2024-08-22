1c2cef8a11e69d0337dd50116ca82937ac7c550b
gabriel.silva98 <gabriel.silva98@BRE203D25>
2024-08-22 19:39:32 -0300

    Adicionar arquivos de projeto.

---
+++ Empresa.Endereco.Api/Controllers/EnderecoController.cs
@@ -0,0 +1,17 @@
+﻿using Empresa.Endereco.Servico.ViaCep;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empresa.Endereco.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController:ControllerBase
+    {
+        [HttpGet(Name ="GetEndereco")]
+        public async Task<IActionResult> ObterEnderecos(string cep)
+        {
+            var requisicao = await new ViaCepService().ObterEnderecoPorCep(cep);
+            return Ok(requisicao);
+        }
+    }
+}
---
+++ Empresa.Endereco.Servico/ViaCep/ViaCepService.cs
@@ -0,0 +1,29 @@
+﻿using Empresa.Endereco.Dominio.Entites;
+using Newtonsoft.Json;
+
+namespace Empresa.Endereco.Servico.ViaCep
+{
+    public class ViaCepService
+    {
+        //Função assincrona que irá comunicar com o servidor da ViaCep
+        public async Task<EnderecoEntity> ObterEnderecoPorCep(string cep)
+        {
+            //Abrindo uma comunicação de protocolo HTTP para comunicar com outro servidor
+            var httpClient = new HttpClient();
+
+            //Executando a operação de requisição para a rota da ViaCep, passando o CEP de forma dinâmica utilizando formatação de string
+            var retornoRequisicao = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json");
+
+            //Verificando se a requisição respondeu com sucesso (status code 200)
+            if(retornoRequisicao.IsSuccessStatusCode)
+            {
+                //Deu sucesso, conseguiu comunicar com a ViaCep
+                //Obter a informação retornada pela API
+                var objetoSerializado = await retornoRequisicao.Content.ReadAsStringAsync();
+                //Comando para desserializar o objeto serializado, recuperado do retorno da requisição da ViaCep
+                return JsonConvert.DeserializeObject<EnderecoEntity>(objetoSerializado);
+            }
+            return new EnderecoEntity();
+        }
+    }
+}
